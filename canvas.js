ee6fb5c52a7c5f747ef2aa2895e146a640583eb9
David Arellano <dtarellano1@gmail.com>
2017-08-02 20:02:16 -0700

    created multiple x and y coordinate pixels

--- canvas.js
+++ canvas.js
@@ -5,55 +5,70 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext("2d");
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
 var dx = 4;
 var dy = 4;
 
-function animeX(x, dx) {
-   this.x = x;
-   this.dx = dy;
+// GLOBAL X PIXELS
+function animeX(dx, y) {
+   this.dx = dx;
+   this.y = y;
+   var xx = 30;
 
    this.create = function() {
       c.fillStyle = "rgba(0, 255, 0, 0.5)";
-      c.fillRect(this.x, 300, 40, 40);
+      c.fillRect(xx, this.y, 30, 30);
    };
 
    this.go = function() {
-      if (this.x > innerWidth || this.x < 0) {
+      if (xx + 30 > innerWidth || xx <= 0) {
          this.dx = -this.dx;
       }
-      this.x += this.dx;
+      xx += this.dx;
       this.create();
    };
 }
 
-function animeY(y, dy) {
-   this.y = y;
+// GLOBALE Y PIXELS
+function animeY(dy, x) {
    this.dy = dy;
+   this.x = x;
+   var yy = 30;
 
    this.create = function() {
       c.fillStyle = "rgba(0, 255, 0, 0.5)";
-      c.fillRect(300, this.y, 40, 40);
+      c.fillRect(this.x, yy, 30, 30);
    };
 
    this.go = function() {
-      if (this.y > innerHeight || this.y < 0) {
+      if (yy + 30 > innerHeight || yy < 0) {
          this.dy = -this.dy;
       }
-      this.y += this.dy;
+      yy += this.dy;
       this.create();
    };
 }
+// CREATING PIXELS
+var Xpixels = [];
+var Ypixels = [];
 
-var anime = new animeX(x, dx);
+function arrayGenerator(array, pixel) {
+   for (var i = 0; i < 4; i++) {
+      var x = Math.random() * innerWidth;
+      var y = Math.random() * innerHeight;
+      array.push(new pixel(dx, y));
+   }
+}
 
-var animationY = new animeY(y, dy);
-
+arrayGenerator(Xpixels, animeX);
+arrayGenerator(Ypixels, animeY);
+// DISPLAY PIXELS
 function animate() {
    window.requestAnimationFrame(animate);
    c.clearRect(0, 0, innerWidth, innerHeight);
-   anime.go();
-   animationY.go();
+
+   for (var i = 0; i < 4; i++) {
+      Xpixels[i].go();
+      Ypixels[i].go();
+   }
 }
 animate();
